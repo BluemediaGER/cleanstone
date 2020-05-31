365b5aec13c8b5355f4428021167afde61f43d9e
BluemediaGER <oliver@traber-info.de>
2020-06-01 00:21:44 +0200

    Add main method in CleanStone.java

--- src/main/java/de/traber_info/home/cleanstone/CleanStone.java
+++ src/main/java/de/traber_info/home/cleanstone/CleanStone.java
@@ -1,5 +1,14 @@
 package de.traber_info.home.cleanstone;
 
+import de.traber_info.home.cleanstone.model.config.BackendServerMapping;
+import de.traber_info.home.cleanstone.proxy.CleanstoneProxy;
+import de.traber_info.home.cleanstone.util.ConfigUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Main class for cleanstone. Performs the first initialisation of all components.
  *
@@ -7,6 +16,32 @@ package de.traber_info.home.cleanstone;
  */
 public class CleanStone {
 
+    /** SLF4J logger for usage in this class */
+    private static final Logger LOG = LoggerFactory.getLogger(CleanStone.class.getName());
 
+    /** Map that assigns domain names to their corresponding backend servers */
+    private static Map<String, BackendServerMapping> backendServerMappings = new HashMap<>();
+
+    /**
+     * Main function of cleanstone. Initializes everything that is needed to run cleanstone.
+     * @param args Arguments passed in by the commandline.
+     */
+    public static void main(String[] args) {
+        for (BackendServerMapping backendServerMapping : ConfigUtil.getConfig().getBackendServerMappings()) {
+            backendServerMappings.put(backendServerMapping.getMappingDomain(), backendServerMapping);
+        }
+
+        LOG.info("Starting proxy server on port {}", ConfigUtil.getConfig().getListenPort());
+        CleanstoneProxy cleanstoneProxy = new CleanstoneProxy(ConfigUtil.getConfig().getListenPort());
+        cleanstoneProxy.listen();
+    }
+
+    /**
+     * Get the map that assigns domain names to their corresponding backend servers.
+     * @return Map that assigns domain names to their corresponding backend servers.
+     */
+    public static Map<String, BackendServerMapping> getBackendServerMappings() {
+        return backendServerMappings;
+    }
 
 }
