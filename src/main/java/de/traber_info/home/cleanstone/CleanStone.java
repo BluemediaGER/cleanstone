b6ed0c99796e0363557d3dc4f3896fcdee9e5833
BluemediaGER <oliver@traber-info.de>
2020-05-31 20:23:35 +0200

    Initial commit

---
+++ src/main/java/de/traber_info/home/cleanstone/CleanStone.java
@@ -0,0 +1,12 @@
+package de.traber_info.home.cleanstone;
+
+/**
+ * Main class for cleanstone. Performs the first initialisation of all components.
+ *
+ * @author Oliver Traber
+ */
+public class CleanStone {
+
+
+
+}
